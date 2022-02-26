67e5c0f5737eddfbe53ac8523e29b30c4c64d215
Ankook <52046510+Ankook@users.noreply.github.com>
2022-02-26 23:09:47 +0300

    we cant use basename with hashrouter

--- src/App.js
+++ src/App.js
@@ -68,7 +68,7 @@ const mapStateToProps = (state) => ({
     connect(mapStateToProps, { initializeApp }))(App);
   
 const SamuraiJSApp = (props) => {
-  return <HashRouter basename={process.env.PUBLIC_URL}>
+  return <HashRouter>
       <Provider store={store}>
         <AppContainer />
       </Provider>
