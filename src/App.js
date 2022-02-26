e6d333a04ac7bb7b5990cee95db78f77744c0672
Ankook <52046510+Ankook@users.noreply.github.com>
2022-02-26 22:20:38 +0300

    fixing BroserRouter path bug for public version of social network

--- src/App.js
+++ src/App.js
@@ -68,7 +68,7 @@ const mapStateToProps = (state) => ({
     connect(mapStateToProps, { initializeApp }))(App);
   
 const SamuraiJSApp = (props) => {
-  return <BrowserRouter>
+  return <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Provider store={store}>
         <AppContainer />
       </Provider>
